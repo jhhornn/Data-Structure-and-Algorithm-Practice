9a9f0897c2c7ef22d11fc4b77814b19f2d156b9b
Awosise Oluwaseun <awosiseo@gmail.com>
2022-11-21 23:26:32 +0100

    bubbleSort Algorithm function

---
+++ src/bubbleSort.js
@@ -0,0 +1,16 @@
+function bubbleSort(array) {
+    let unsorted_until_index = array.length
+    let sorted = false
+
+    while (!sorted) {
+        sorted = true
+        for (let i = 0; i < unsorted_until_index; i++) {
+            if (array[i] > array[i + 1]) {
+                array[i], array[i + 1] = array[i + 1], array[i]
+                sorted = false
+            }
+        }
+        unsorted_until_index -= 1
+    }
+    return array
+}
